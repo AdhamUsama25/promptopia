1b3d6a6e2d3987389bc724d07cd50b74b4628ff0
AdhamUsama25 <adhamusama25@gmail.com>
2024-09-21 00:46:06 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+import { ReactNode } from "react";
+
+export const metadata = {
+  title: "Promptobia",
+  describetion: "Discover & Share AI Prombts",
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider session={undefined}>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
